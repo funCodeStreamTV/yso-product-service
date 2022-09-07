060a91d9a570b68823ec12bc77e7de2b7d2b4cd5
funcodestreamtv <funcodestreamtv@gmail.com>
2022-09-07 19:04:46 +0300

    yso p-s 1 Create project

---
+++ src/main/java/com/aincorp/ysoproductservice/YsoProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.aincorp.ysoproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YsoProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YsoProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aincorp/ysoproductservice/YsoProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aincorp.ysoproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YsoProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
